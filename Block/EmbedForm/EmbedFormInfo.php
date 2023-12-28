9ffc7603c4e512d6daa0f031e55aac2d2d1f5e9d
Franklin <franklin.carrero@conekta.com>
2023-12-28 10:22:08 -0500

    initial commit

---
+++ Block/EmbedForm/EmbedFormInfo.php
@@ -0,0 +1,167 @@
+<?php
+namespace Femsa\Payments\Block\EmbedForm;
+
+use Femsa\Payments\Model\Ui\EmbedForm\ConfigProvider;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Payment\Block\Info;
+use Magento\Payment\Model\Config;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Phrase;
+
+class EmbedFormInfo extends Info
+{
+    /**
+     * @var Config
+     */
+    protected Config $_paymentConfig;
+
+    /**
+     * @var string
+     */
+    protected $_template = 'Femsa_Payments::info/embedform.phtml';
+
+    /**
+     * @param Context $context
+     * @param Config $paymentConfig
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Config $paymentConfig,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->_paymentConfig = $paymentConfig;
+    }
+
+    /**
+     * Get CC type name
+     *
+     * @return Phrase|mixed
+     * @throws LocalizedException
+     */
+    public function getCcTypeName()
+    {
+        $types = $this->_paymentConfig->getCcTypes();
+        $ccType = $this->getInfo()->getCcType();
+        if (isset($types[$ccType])) {
+            return $types[$ccType];
+        }
+        return empty($ccType) ? __('N/A') : $ccType;
+    }
+
+    /**
+     * Get additional Data
+     *
+     * @return mixed
+     * @throws LocalizedException
+     */
+    public function getAdditionalData()
+    {
+        return $this->getInfo()->getAdditionalInformation();
+    }
+
+    /**
+     * Get off-line info
+     *
+     * @return false|mixed
+     * @throws LocalizedException
+     */
+    public function getOfflineInfo()
+    {
+        $additional_data = $this->getAdditionalData();
+        if (isset($additional_data['offline_info']['data'])) {
+            return $additional_data['offline_info']['data'];
+        }
+
+        return false;
+    }
+
+    /**
+     * Get payment method type
+     *
+     * @return mixed
+     * @throws LocalizedException
+     */
+    public function getPaymentMethodType()
+    {
+        return $this->getInfo()->getAdditionalInformation('payment_method');
+    }
+
+    /**
+     * Get payment method title
+     *
+     * @return string
+     * @throws LocalizedException
+     */
+    public function getPaymentMethodTitle(): string
+    {
+        $methodType = $this->getPaymentMethodType();
+        $title = '';
+
+        switch ($methodType) {
+            case ConfigProvider::PAYMENT_METHOD_CREDIT_CARD:
+                $title = 'Pago con Tarjeta';
+                break;
+            
+            case ConfigProvider::PAYMENT_METHOD_CASH:
+                $title = 'Pago en Efectivo';
+                break;
+            case ConfigProvider::PAYMENT_METHOD_BANK_TRANSFER:
+                $title = 'Transferencia Bancaria';
+                break;
+        }
+
+        return $title;
+    }
+
+    /**
+     * Is credit card payment method
+     *
+     * @return bool
+     * @throws LocalizedException
+     */
+    public function isCreditCardPaymentMethod(): bool
+    {
+        return $this->getPaymentMethodType() === ConfigProvider::PAYMENT_METHOD_CREDIT_CARD;
+    }
+
+    /**
+     * Is cash payment method
+     *
+     * @return bool
+     * @throws LocalizedException
+     */
+    public function isCashPaymentMethod(): bool
+    {
+        return $this->getPaymentMethodType() === ConfigProvider::PAYMENT_METHOD_CASH;
+    }
+
+    /**
+     * Is BankTransfer payment method
+     *
+     * @return bool
+     * @throws LocalizedException
+     */
+    public function isBankTransferPaymentMethod(): bool
+    {
+        return $this->getPaymentMethodType() === ConfigProvider::PAYMENT_METHOD_BANK_TRANSFER;
+    }
+
+    /**
+     * Show if card is debit o credit card
+     *
+     * @return string
+     * @throws LocalizedException
+     */
+    public function getCardType(): ?string
+    {
+        $additionalData = $this->getAdditionalData();
+    
+        if (isset($additionalData['card_type'])) {
+            return $additionalData['card_type'];
+        }
+        
+        return null;
+    }
+}
---
+++ Observer/DataAssignObserver.php
@@ -0,0 +1,97 @@
+<?php
+namespace Femsa\Payments\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Payment\Observer\AbstractDataAssignObserver;
+use Magento\Quote\Api\Data\PaymentInterface;
+use Magento\Checkout\Model\Session;
+
+class DataAssignObserver extends AbstractDataAssignObserver
+{
+    public const PAYMENT_METHOD = 'payment_method';
+    public const CC_TYPE = 'cc_type';
+    public const CC_EXP_YEAR = 'cc_exp_year';
+    public const CC_EXP_MONTH = 'cc_exp_month';
+    public const CC_BIN = 'cc_bin';
+    public const CC_LAST_4 = 'cc_last_4';
+    public const CARD_TOKEN = 'card_token';
+    public const MONTHLY_INSTALLMENTS = 'monthly_installments';
+    public const SAVED_CARD = 'saved_card';
+    public const SAVED_CARD_LATER = 'saved_card_later';
+    public const IFRAME_PAYMENT = 'iframe_payment';
+    public const ORDER_ID = 'order_id';
+    public const TXN_ID = 'txn_id';
+    public const CARD_TYPE = 'card_type';
+    public const REFERENCE = 'reference';
+    /**
+     * @var string[]
+     */
+    protected array $additionalInformationList = [
+        self::PAYMENT_METHOD,
+        self::CC_TYPE,
+        self::CARD_TYPE,
+        self::CC_EXP_YEAR,
+        self::CC_EXP_MONTH,
+        self::CC_BIN,
+        self::CC_LAST_4,
+        self::CARD_TOKEN,
+        self::MONTHLY_INSTALLMENTS,
+        self::SAVED_CARD,
+        self::SAVED_CARD_LATER,
+        self::IFRAME_PAYMENT,
+        self::ORDER_ID,
+        self::TXN_ID,
+        self::REFERENCE,
+    ];
+    /**
+     * @var Session
+     */
+    protected Session $_checkoutSession;
+
+    /**
+     * @param Session $checkoutSession
+     */
+    public function __construct(
+        Session $checkoutSession
+    ) {
+        $this->_checkoutSession = $checkoutSession;
+    }
+
+    /**
+     * Execute
+     *
+     * @param Observer $observer
+     * @return void
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
+    public function execute(Observer $observer)
+    {
+        $data = $this->readDataArgument($observer);
+
+        $additionalData = $data->getData(PaymentInterface::KEY_ADDITIONAL_DATA);
+
+        if (!is_array($additionalData)) {
+            return;
+        }
+
+        $paymentInfo = $this->readPaymentModelArgument($observer);
+        $quote = $this->_checkoutSession->getQuote();
+
+        $paymentInfo->setAdditionalInformation(
+            'quote_id',
+            $quote->getId()
+        );
+
+        foreach ($this->additionalInformationList as $additionalInformationKey) {
+            if (isset($additionalData[$additionalInformationKey])) {
+                $paymentInfo->setAdditionalInformation(
+                    $additionalInformationKey,
+                    $additionalData[$additionalInformationKey]
+                );
+            }
+        }
+    }
+}
